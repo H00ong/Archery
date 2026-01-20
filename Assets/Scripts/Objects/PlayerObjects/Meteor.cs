48f96a5b5c01a7fd7f29efddb94b9fdb2698aeca
박재홍 <pjhbbogri@gmail.com>
2026-01-20 11:02:31 +0900

    refactor: prevent duplicate hits in Meteor using HashSet and GetComponentInParent

--- Assets/Scripts/Objects/PlayerObjects/Meteor.cs
+++ Assets/Scripts/Objects/PlayerObjects/Meteor.cs
@@ -1,39 +1,57 @@
 using System.Collections;
+using System.Collections.Generic;
 using Enemy;
 using Managers;
 using Players;
 using UnityEngine;
 
+public readonly struct MeteorInitConfig
+{
+    public readonly Vector3 Position;
+    public readonly int Damage;
+    public readonly BarrelType Type;
+
+    public MeteorInitConfig(Vector3 position, int damage, BarrelType type)
+    {
+        Position = position;
+        Damage = damage;
+        Type = type;
+    }
+}
+
 public class Meteor : MonoBehaviour
 {
-    [SerializeField] float _terminateTime = 2f;
-    [SerializeField] float _timeDelayForMeteor = 1f;
+    [SerializeField] float terminateTime = 2f;
+    [SerializeField] float timeDelayForMeteor = 1f;
 
-    [SerializeField] LayerMask _enemyLayer;
-    [SerializeField] float _attackRange = 1f;
+    [SerializeField] LayerMask enemyLayer;
+    [SerializeField] float attackRange = 1f;
 
     int _atk;
     BarrelType _barrelType;
     float _timer = 0f;
+    
+    private HashSet<IDamageable> _hitEnemies = new HashSet<IDamageable>();
 
     private void OnEnable()
     {
         StartCoroutine(TerminateCoroutine());
+        _hitEnemies.Clear();
     }
 
-    public void Init(Vector3 pos, int atk, BarrelType barrelType = BarrelType.Common)
+    public void Initialize(MeteorInitConfig config)
     {
-        transform.position = pos;
+        transform.position = config.Position;
         transform.rotation = Quaternion.identity;
 
-        _atk = atk;
-        _barrelType = barrelType;
+        _atk = config.Damage;
+        _barrelType = config.Type;
         _timer = 0f;
     }
 
     private void Update()
     {
-        if (_timer >= _timeDelayForMeteor)
+        if (_timer >= timeDelayForMeteor)
             AttackCheck();
         else
             _timer += Time.deltaTime;
@@ -41,37 +59,40 @@ public class Meteor : MonoBehaviour
 
     private void AttackCheck()
     {
-        var cds = Physics.OverlapSphere(transform.position, _attackRange, _enemyLayer);
+        var cds = Physics.OverlapSphere(transform.position, attackRange, enemyLayer);
 
         if (cds.Length > 0)
         {
             foreach (var cd in cds)
             {
-                if (cd.TryGetComponent<IDamageable>(out var enemy))
+                var enemy = cd.GetComponentInParent<IDamageable>();
+                
+                if (enemy != null && _hitEnemies.Add(enemy))
                 {
-                    DamageType damageType = ConvertBarrelTypeToDamageType(_barrelType);
+                    EffectType damageType = ConvertBarrelTypeToDamageType(_barrelType);
                     var damageInfo = new DamageInfo(_atk, damageType, gameObject);
                     damageInfo.hitPoint = cd.ClosestPoint(transform.position);
                     enemy.TakeDamage(damageInfo);
+                    Debug.Log($"[Meteor] Hit {cd.gameObject.name} | Type: {_barrelType} | Damage: {_atk}");
                 }
             }
         }
     }
 
-    private DamageType ConvertBarrelTypeToDamageType(BarrelType type)
+    private EffectType ConvertBarrelTypeToDamageType(BarrelType type)
     {
         return type switch
         {
-            BarrelType.Venom => DamageType.Venom,
-            BarrelType.Blaze => DamageType.Fire,
-            BarrelType.Ice => DamageType.Ice,
-            _ => DamageType.Normal
+            BarrelType.Venom => EffectType.Venom,
+            BarrelType.Blaze => EffectType.Fire,
+            BarrelType.Ice => EffectType.Ice,
+            _ => EffectType.Normal
         };
     }
 
     IEnumerator TerminateCoroutine()
     {
-        yield return new WaitForSeconds(_terminateTime);
+        yield return new WaitForSeconds(terminateTime);
 
         _timer = 0f;
         PoolManager.Instance.ReturnObject(gameObject);
@@ -81,6 +102,6 @@ public class Meteor : MonoBehaviour
     {
         Gizmos.color = Color.cyan;
 
-        Gizmos.DrawWireSphere(transform.position, _attackRange);
+        Gizmos.DrawWireSphere(transform.position, attackRange);
     }
 }
