990f7bf72e6238a68b6d974ad21fa00a36d8d686
박재홍 <pjhbbogri@gmail.com>
2026-01-23 15:42:45 +0900

    feat: Utils에 Flags 처리 및 타입 변환 유틸리티 추가

--- Assets/Scripts/Utils/Utils.cs
+++ Assets/Scripts/Utils/Utils.cs
@@ -14,6 +14,18 @@ public enum TagType
 
 public static class Utils
 {
+    private static readonly Dictionary<TagType, string> Map = new()
+    {
+        { TagType.Player,           "Player" },
+        { TagType.Enemy,            "Enemy" },
+        { TagType.Obstacle,         "Obstacle" },
+        { TagType.PlayerProjectile, "PlayerProjectile" },
+        { TagType.EnemyProjectile,  "EnemyProjectile" },
+        { TagType.Floor,            "Floor" },
+    };
+
+    public static string ToString(TagType t) => Map[t];
+
     public static Component GetOrAddComponent(this GameObject obj, Type type)
     {
         var comp = obj.GetComponent(type);
@@ -59,15 +71,31 @@ public static class Utils
         return new Vector3(pos.x, 0, pos.z);
     }
 
-    private static readonly Dictionary<TagType, string> Map = new()
-    {
-        { TagType.Player,           "Player" },
-        { TagType.Enemy,            "Enemy" },
-        { TagType.Obstacle,         "Obstacle" },
-        { TagType.PlayerProjectile, "PlayerProjectile" },
-        { TagType.EnemyProjectile,  "EnemyProjectile" },
-        { TagType.Floor,            "Floor" },
-    };
+    public static bool HasEffectType(EffectType mask, EffectType t) => (mask & t) != 0;
+    
+    public static bool HasFlag(Enemy.EnemyTag mask, Enemy.EnemyTag t) => (mask & t) != 0;
 
-    public static string ToString(TagType t) => Map[t];    
+    public static EffectType BarrelTypeToEffectType(Players.BarrelType type)
+    {
+        return type switch
+        {
+            Players.BarrelType.Venom => EffectType.Poison,
+            Players.BarrelType.Blaze => EffectType.Fire,
+            Players.BarrelType.Ice => EffectType.Ice,
+            Players.BarrelType.Lightning => EffectType.Lightning,
+            _ => EffectType.Normal
+        };
+    }
+
+    public static EffectType OrbTypeToEffectType(Players.OrbType type)
+    {
+        return type switch
+        {
+            Players.OrbType.Venom => EffectType.Poison,
+            Players.OrbType.Blaze => EffectType.Fire,
+            Players.OrbType.Ice => EffectType.Ice,
+            Players.OrbType.Lightning => EffectType.Lightning,
+            _ => EffectType.Normal
+        };
+    }
 }
