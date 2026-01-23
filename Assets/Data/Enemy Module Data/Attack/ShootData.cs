9964d1a4dd8171955156a40140c1ef6dec9dba4d
박재홍 <pjhbbogri@gmail.com>
2026-01-23 15:42:55 +0900

    fix: ShootData의 GetEffectType을 Flags 방식으로 개선 및 OnValidate 수정

--- Assets/Data/Enemy Module Data/Attack/ShootData.cs
+++ Assets/Data/Enemy Module Data/Attack/ShootData.cs
@@ -10,21 +10,37 @@ public class ShootData : AttackModuleData
     public AssetReferenceGameObject projectilePrefab;
 
     [Header("Firing Settings")]
+    public bool playerTargeting;
     public EnemyPointType firePointType;
-    
-    private void OnEnable()
-    {
-        targetTag = EnemyTag.Shoot;
-    }
     
     public List<Transform> GetShootingPoint(EnemyController controller)
     {
         return controller.enemyReference != null ? controller.enemyReference.GetPoints(EnemyPointType.NormalShootingMuzzle) : null;
     }
 
+    public EffectType GetEffectType()
+    {
+        EnemyTag attributeTags = targetTag & EnemyTag.AttributeMask;
+        EffectType result = EffectType.Normal;
+
+        if (Utils.HasFlag(attributeTags, EnemyTag.Fire))
+            result |= EffectType.Fire;
+        if (Utils.HasFlag(attributeTags, EnemyTag.Ice))
+            result |= EffectType.Ice;
+        if (Utils.HasFlag(attributeTags, EnemyTag.Poison))
+            result |= EffectType.Poison;
+        if (Utils.HasFlag(attributeTags, EnemyTag.Lightning))
+            result |= EffectType.Lightning;
+        if (Utils.HasFlag(attributeTags, EnemyTag.Magma))
+            result |= EffectType.Magma;
+
+        return result;
+    }
+
     private void OnValidate()
     {
-        targetTag = EnemyTag.Shoot;
+        // Shoot 태그를 항상 포함, 다른 플래그는 유지
+        targetTag |= EnemyTag.Shoot;
         firePointType = EnemyPointType.NormalShootingMuzzle;
     }
 }
