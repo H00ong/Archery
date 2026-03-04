7d9b3c640fa4bb7119e9276abef7e505e650af06
박재홍 <pjhbbogri@gmail.com>
2026-03-04 09:32:39 +0900

    feat: PlayerManager 싱글톤 추가 (PlayerData 보관), PlayerContainer 마커 컴포넌트 추가

---
+++ Assets/Scripts/Managers/PlayerManager.cs
@@ -0,0 +1,35 @@
+using Players;
+using UnityEngine;
+
+namespace Managers
+{
+    public class PlayerManager : MonoBehaviour
+    {
+        public static PlayerManager Instance;
+        public PlayerData PlayerData { get; private set; }
+
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+            else if (Instance != this)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        public void InitializePlayerData()
+        {
+            PlayerData = DataManager.Instance.GetPlayerData();
+        }
+
+        public void SyncPlayerData(PlayerData playerData)
+        {
+            PlayerData = playerData;
+        }
+    }
+}
+
