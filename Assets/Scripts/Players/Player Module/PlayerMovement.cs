108fcdbfef1f1f89752618fac600752efe536796
박재홍 <pjhbbogri@gmail.com>
2026-03-08 11:09:15 +0900

    refac: PlayerMovement를 Rigidbody 물리 기반 이동으로 전환

--- Assets/Scripts/Players/Player Module/PlayerMovement.cs
+++ Assets/Scripts/Players/Player Module/PlayerMovement.cs
@@ -1,47 +1,83 @@
-using Game.Player;
+using Managers;
 using Players;
 using Stat;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [Header("Player Movement Info")]
-    [SerializeField] float sphereCastRadius = 0.5f;
-    [SerializeField] LayerMask obstacleLayer;
+    private const float threshold = 0.01f; // 입력이 너무 작은 경우 무시하기 위한 임계값
 
-    private PlayerStat _stat = null;
-    private PlayerController _playerManager = null;
+    [SerializeField] private Rigidbody playerRigidbody;
 
-    public void ExecuteMovement(Vector2 moveInput)
+    private PlayerController _playerController;
+    private PlayerAttack _playerAttack;
+    private PlayerStat _stat;
+    private InputManager _inputManager;
+    private Vector3 _currentMoveDir; // InputManager로부터 전달받은 이동 방향
+
+    private void Awake()
     {
-        _stat ??= PlayerController.Instance.Stat;
-        _playerManager ??= PlayerController.Instance;
+        playerRigidbody = GetComponent<Rigidbody>();
 
-        _playerManager.ChangePlayerAnimation(PlayerState.Move);
-
-        Vector3 moveDir = new Vector3(moveInput.x, 0, moveInput.y);
-        transform.rotation = Quaternion.LookRotation(moveDir, Vector3.up);
-
-        Vector3 collisionPos = transform.position + transform.up + transform.forward * .5f;
-        if (Physics.OverlapSphere(collisionPos, sphereCastRadius, obstacleLayer).Length > 0)
-            return;
-
-        transform.position += moveDir * _stat.MoveSpeed * Time.deltaTime;
+        ApplyRigidbodySettings();
     }
 
-    #region Skill Methods
+    void Start()
+    {
+        _stat = PlayerController.Instance.Stat;
+        _playerController = PlayerController.Instance;
+        _playerAttack = PlayerController.Instance.Attack;
+        _inputManager = InputManager.Instance;
+    }
 
+    private void ApplyRigidbodySettings()
+    {
+        playerRigidbody.constraints = RigidbodyConstraints.FreezePositionY |
+                          RigidbodyConstraints.FreezeRotation;
+
+        playerRigidbody.mass = 9999f;
+    }
+
+    void Update()
+    {
+        SetMoveInput(_inputManager.MoveInput);
+    }
+
+    private void SetMoveInput(Vector2 moveInput)
+    {
+        _currentMoveDir = new Vector3(moveInput.x, 0, moveInput.y);
+
+        if (_currentMoveDir != Vector3.zero)
+        {
+            transform.rotation = Quaternion.LookRotation(_currentMoveDir, Vector3.up);
+            _playerController.ChangePlayerAnimation(PlayerState.Move);
+        }
+        else
+        {
+            if (EnemyManager.Instance.Enemies.Count > 0)
+            {
+                _playerController.ChangePlayerAnimation(PlayerState.Attack);
+                _playerAttack.Attack();
+            }
+            else
+            {
+                _playerController.ChangePlayerAnimation(PlayerState.Idle);
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (_currentMoveDir == Vector3.zero)
+            return;
+
+        Vector3 targetPos = playerRigidbody.position + _currentMoveDir * _stat.MoveSpeed * Time.fixedDeltaTime;
+
+        playerRigidbody.MovePosition(targetPos);
+    }
+    
     public void UpdateMoveSpeed(float modifier)
     {
         _stat.ApplyMoveSpeedModifier(modifier);
-    }
-
-    #endregion
-
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.green;
-
-        Gizmos.DrawWireSphere(transform.position + transform.up + transform.forward * .5f, sphereCastRadius);
     }
 }
