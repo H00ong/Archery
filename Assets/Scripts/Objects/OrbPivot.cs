f6d8c2d4e9746beff399d1987c5824d28edf6eff
박재홍 <pjhbbogri@gmail.com>
2026-03-21 14:21:46 +0900

    refac: OrbPivot 위치 추적을 Rigidbody 기반으로 변경
    - Transform 캐싱 대신 Rigidbody.position 사용으로 물리 기반 위치 동기화

--- Assets/Scripts/Objects/OrbPivot.cs
+++ Assets/Scripts/Objects/OrbPivot.cs
@@ -1,16 +1,20 @@
 using Players;
+using UnityEditor;
 using UnityEngine;
 
 public class OrbPivot : MonoBehaviour
 {
-    private Transform _pivot;
+    private Vector3 _pivotPos;
 
     private void Update()
     {
         if (PlayerController.Instance == null)
             return;
+
+        PlayerController player = PlayerController.Instance;
+        Rigidbody playerRigidbody = player.GetComponent<Rigidbody>();
+        _pivotPos = playerRigidbody.position;
         
-        _pivot ??= PlayerController.Instance.transform;
-        transform.position = new Vector3(_pivot.position.x, transform.position.y, _pivot.position.z);
+        transform.position = new Vector3(_pivotPos.x, transform.position.y, _pivotPos.z);
     }
 }
