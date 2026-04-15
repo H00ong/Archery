cd06b598d29e4122826455271ddfc6f6e3ad6257
박재홍 <pjhbbogri@gmail.com>
2026-04-15 10:57:40 +0900

    chore: 캐릭터 에셋 데이터 및 씬 설정 조정

--- Assets/Scripts/Managers/DataManager.cs
+++ Assets/Scripts/Managers/DataManager.cs
@@ -149,7 +149,7 @@ namespace Managers
         public void LoadPlayerData()
         {
             // TODO : Load PlayerData from SaveSystem
-            playerData = new PlayerData("BlueWizard", 500);
+            playerData = new PlayerData("BlueWizard", 5000);
         }
 
         public PlayerData GetPlayerData()
